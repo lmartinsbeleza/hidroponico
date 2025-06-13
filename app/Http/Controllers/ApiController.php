1d2c33b24c109a417a40d222e4e4b7298f0e4637
Luiz <lmartisnbeleza@gmail.com>
2025-06-13 18:01:33 -0400

    Criando informações do banco de dados

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Dados;
+use App\Models\Hidroponico;
+use App\Models\User;
+use App\Models\UsersHidroponia;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function __construct(
+        private User $user,
+        private Dados $dados,
+        private Hidroponico $hidroponico,
+        private UsersHidroponia $usersHidroponia
+    ) {
+        $this->userMD = $user;
+        $this->dadosMD = $dados;
+        $this->hidroponicoMD = $hidroponico;
+        $this->userHidroponiaMD = $usersHidroponia;
+    }
+
+    public function getData(Request $req) {
+        return response()->json($req->all(), 200);
+    }
 }
---
+++ database/seeders/HidroponicoSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class HidroponicoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
